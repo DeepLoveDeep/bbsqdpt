1b22b078ec0b893660d48f6daa4859863fb34541
scq <524579334@qq.com>
2018-10-31 16:47:39 +0800

    测试

--- src/main/java/cc/mrbird/Application.java
+++ src/main/java/cc/mrbird/Application.java
@@ -27,6 +27,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
+
         log.info("BBS started up successfully at {} {}", LocalDate.now(), LocalTime.now());
     }
 }
